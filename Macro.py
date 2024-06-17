ab0e7478e60f3a78d236b68ce617d18d7d21edf6
huggywuggy1289 <bangstation0714@naver.com>
2024-06-17 14:38:36 +0900

    자동방지입력문자만 남음

--- Macro.py
+++ Macro.py
@@ -4,9 +4,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import cv2
+import numpy as np
+import pytesseract
+from pytesseract import image_to_string
+from PIL import Image
+from captcha.image import ImageCaptcha
 
-# 7.16 A구역
-XPATH = '/html/body/div[4]/table/tbody/tr/td[3]/div/div/table[2]/tbody/tr[3]/td[3]/ul/li[1]/button'
+# 자동방지입력문자
+pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\손재윤\\OneDrive\\바탕 화면\\tesseract-5.4.1\\tesseract.exe"
+
+# A구역
+XPATH = '/html/body/div[4]/table/tbody/tr/td[3]/div/div/table[2]/tbody/tr[3]/td[4]/ul/li[1]/button'
 
 def clock(target_time):
     while True:
@@ -36,8 +45,6 @@ try:
     url = 'https://camping.gtdc.or.kr/DZ_reservation/reserCamping_v3.php?xch=reservation&xid=camping_reservation&sdate=202407'
     browser.get(url)
 
-    clock("10:00") #함수 적용
-
     # 팝업 처리
     popup_xpath = "//input[@value='Y']"
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, popup_xpath)))
@@ -48,8 +55,10 @@ try:
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, popup2_xpath)))
     browser.execute_script("arguments[0].click();", browser.find_element(By.XPATH, popup2_xpath))
 
+    clock("10:00") #함수 적용
+
     # 날짜 선택
-    date_xpath = "//button[@value='C:2024-07-15']"
+    date_xpath = "//button[@value='C:2024-07-17']"
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, date_xpath)))
     print("날짜 선택 완료")
 
@@ -60,25 +69,29 @@ try:
         print("A구역 접속 버튼 클릭 실패")
         raise Exception("A구역 접속 버튼 클릭 실패")
 
-    # 특정 위치 버튼 클릭 A134
+    # 특정 위치 버튼 클릭
     try:
-        location_button_xpath = '//span[text()="A132"]/parent::button'
-        job = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, location_button_xpath)))
-        browser.execute_script("arguments[0].scrollIntoView({block: 'center'});", job)
-        time.sleep(1)
-        browser.execute_script("arguments[0].click();", job)
-        print("위치 버튼 클릭 성공")
+        location_button_xpath = ['//span[text()="A147"]/parent::button',
+                                 '//span[text()="A149"]/parent::button',
+                                 '//span[text()="A129"]/parent::button',
+                                 '//span[text()="A128]/parent::button']
+        for path in location_button_xpath:
+            try:
+                job = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, path)))
+                browser.execute_script("arguments[0].scrollIntoView({block: 'center'});", job)
+                browser.execute_script("arguments[0].click();", job)
+                print(f"위치 버튼 클릭 성공: {path}")
+
+                people_xpath = '/html/body/div[4]/table/tbody/tr/td[3]/div/div/table[1]/tbody/tr/td[4]/select/option[5]'
+                browser.find_element(By.XPATH, people_xpath).click()
+                print("인원 지정 성공")
+
+            except Exception as e:
+                print(f"위치 버튼 클릭 실패: {path} - {e}")
+                continue
+
     except Exception as e:
         print(f"위치 버튼 클릭 과정에서 오류 발생: {e}")
-
-    # 인원(4명) 지정
-    try:
-        people_xpath = '/html/body/div[4]/table/tbody/tr/td[3]/div/div/table[1]/tbody/tr/td[4]/select/option[5]'
-        browser.find_element(By.XPATH, people_xpath).click()
-        print("인원 지정 성공")
-    except Exception as e:
-        print(f"인원 지정 과정에서 오류 발생: {e}")
-
     # 3박 4일
     try:
         stay_xpath = '/html/body/div[4]/table/tbody/tr/td[3]/div/div/div[5]/select/option[3]'
@@ -88,6 +101,28 @@ try:
         print(f"숙박 기간 지정 과정에서 오류 발생: {e}")
 
     print("무사히 지정 완료!")
+
+    try:
+        # 캡차 이미지 찾기 및 저장
+        captcha_image = browser.find_element(By.XPATH, '/html/body/div[4]/table/tbody/tr/td[3]/div/div/table[3]/tbody/tr/td/img')
+        captcha_image.screenshot('captcha.png')
+
+        # OCR추가: 단일문자 인식 --psm 6 (참고: https://m.blog.naver.com/johnsmithbrainseven/222242853850)
+        custom_config = r'--oem 3 --psm 6 -c tessedit_char_whitelist=0123456789'
+        captcha_text = pytesseract.image_to_string(Image.open('captcha.png'), config=custom_config)
+
+        # 텍스트 입력
+        captcha_input = browser.find_element(By.XPATH, '/html/body/div[4]/table/tbody/tr/td[3]/div/div/table[3]/tbody/tr/td/input')
+        captcha_input.send_keys(captcha_text)
+        print(f'입력된 캡챠 텍스트: {captcha_input.get_attribute("value")}')
+
+        # 제출 버튼 클릭
+        xpath_next = "/html/body/div[4]/table/tbody/tr/td[3]/div/div/div[6]/button[2]" 
+        browser.find_element(By.XPATH, xpath_next).click()
+        print("자동방지문자 우회성공")
+
+    except Exception as e:
+        print("방지문자 입력과정에서 오류발생!")
 
 except Exception as e:
     print(f"예약 과정에서 오류가 발생하였습니다: {e}")
