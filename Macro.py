1553edc705f1b811871304b56a2a326bdc92c4e3
huggywuggy1289 <bangstation0714@naver.com>
2024-06-21 10:14:59 +0900

    테스트 2번

--- Macro.py
+++ Macro.py
@@ -15,7 +15,7 @@ from captcha.image import ImageCaptcha
 pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\손재윤\\OneDrive\\바탕 화면\\tesseract-5.4.1\\tesseract.exe"
 
 # A구역(수정요망/날짜별로 tr과 td가 다름)
-XPATH = '/html/body/div[4]/table/tbody/tr/td[3]/div/div/table[2]/tbody/tr[3]/td[3]/ul/li[1]/button'
+XPATH = '/html/body/div[4]/table/tbody/tr/td[3]/div/div/table[2]/tbody/tr[4]/td[1]/ul/li[1]/button'
 
 def clock(target_time):
     while True:
@@ -58,7 +58,7 @@ try:
     clock("10:00") #함수 적용
 
     # 날짜 선택(수정요망)
-    date_xpath = "//button[@value='C:2024-07-16']"
+    date_xpath = "//button[@value='C:2024-07-21']"
     WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, date_xpath)))
     print("날짜 선택 완료")
 
@@ -74,7 +74,8 @@ try:
         location_button_xpath = ['/html/body/div[4]/table/tbody/tr/td[3]/div/div/div[4]/div/button[47]',
                                  '/html/body/div[4]/table/tbody/tr/td[3]/div/div/div[4]/div/button[49]',
                                  '/html/body/div[4]/table/tbody/tr/td[3]/div/div/div[4]/div/button[36]',
-                                 '/html/body/div[4]/table/tbody/tr/td[3]/div/div/div[4]/div/button[31]']
+                                 '/html/body/div[4]/table/tbody/tr/td[3]/div/div/div[4]/div/button[31]',
+                                 '/html/body/div[4]/table/tbody/tr/td[3]/div/div/div[4]/div/button[53]']
         for path in location_button_xpath:
             try:
                 job = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, path)))
@@ -97,8 +98,7 @@ try:
     # 3박 4일 : /html/body/div[4]/table/tbody/tr/td[3]/div/div/div[5]/select/option[3]
     try:
         stay_xpath = ['/html/body/div[4]/table/tbody/tr/td[3]/div/div/div[5]/select/option[3]',
-                     '/html/body/div[4]/table/tbody/tr/td[3]/div/div/div[5]/select/option[2]',
-                     '/html/body/div[4]/table/tbody/tr/td[3]/div/div/div[5]/select/option[1]']
+                     '/html/body/div[4]/table/tbody/tr/td[3]/div/div/div[5]/select/option[2]']
         for stay in stay_xpath:
             browser.find_element(By.XPATH, stay_xpath).click()
             print("숙박 기간 지정 성공")
